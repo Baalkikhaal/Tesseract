e5819c6ae5817e6fdb8c0be7d3a5c40a42465a65
Sreekar Guddeti <colonel.sreekar@gmail.com>
2019-08-24 18:10:21 +0100

    Added comments to code

--- convertBook/script.py
+++ convertBook/script.py
@@ -1,3 +1,8 @@
+'''Convert PDF into png using poppler library's pdftocairo Each page is converted to separate png file
+References:
+- [Poppler-Utils](https://packages.ubuntu.com/xenial/poppler-utils)
+- [Poppler for Windows](http://blog.alivate.com.au/poppler-windows/)
+'''
 import os
 filename = 'book.pdf'
 imgFolder = 'images'
--- convertBook/script1.py
+++ convertBook/script1.py
@@ -1,9 +1,14 @@
+'''Convert png to txt using pytesseract module, a python wrapper for Google's Tesseract-OCR library
+References:
+- [Pytesseract](https://github.com/madmaze/pytesseract)
+- [Tesseract HOWTO](https://www.learnopencv.com/deep-learning-based-text-recognition-ocr-using-tesseract-and-opencv/)
+'''
 import pytesseract
 import os
 projectFolder = os.path.abspath(os.curdir)
 outputFile = 'output.txt'
 outputFilePath = os.path.abspath(os.path.join(projectFolder, outputFile))
-if os.path.isfile(outputFilePath): os.remove(outputFilePath) 
+if os.path.isfile(outputFilePath): os.remove(outputFilePath)
 imgFolder = 'images'
 os.chdir(imgFolder)
 images = os.listdir(os.curdir)
